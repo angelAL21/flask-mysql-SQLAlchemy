af37176e735423a59f0c0f8d4167112c506d337e
unknown <angell_2104@hotmail.com>
2022-02-26 10:56:23 -0600

    creating the table and the task schema

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -8,3 +9,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db= SQLAlchemy(app)
 ma = Marshmallow(app)
+
+#task app
+#creating table.
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(100))
+    
+    def __init__(self, title, description):
+        self.description = description
+        self.title = title
+        
+db.create_all()
+
+
+#task schema
+class TaskSchema(ma.Schema):
+    class Meta:
+        fields=('id', 'tittle', 'description')
+        
+task_schema = TaskSchema() #for creating one task.
+task_schema = TaskSchema(many=True) #for creating and getting many tasks.
+
